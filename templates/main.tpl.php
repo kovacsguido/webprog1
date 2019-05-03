045c8a7fb7af2b3ecf90dd8fe49797136de3ef28
Guido Kovacs <kovacs.guido@gmail.com>
2019-05-03 11:27:43 +0200

    Imlemented the home page

--- templates/main.tpl.php
+++ templates/main.tpl.php
@@ -1,3 +1,194 @@
 <h1>Főoldal</h1>
 <hr class="my-4">
-<p>...</p>
+
+<div class="container">
+    <div class="items-row row-0 row clearfix">
+        <div class="col-sm-3">
+            <article class="item column-1 item-featured" itemprop="blogPost" itemscope itemtype="http://schema.org/BlogPosting">
+                <div class="entry-image intro-image" style="height: 140px; max-width: 100%; overflow: hidden;">
+                    <a href="#">
+                        <img src="images/medvematek.jpg" alt="" itemprop="thumbnailUrl" style="max-width: 100%; height: auto;" />
+                    </a>
+                </div>
+                <div class="entry-header">
+                    <dl class="article-info">
+                        <dt class="article-info-term"></dt>
+                        <dd class="create">
+                            <i class="fa fa-clock-o"></i>
+                            <time datetime="2019-04-15T13:28:27+02:00" itemprop="dateCreated" data-toggle="tooltip" title="Létrehozás dátuma"> 2019. április 15.</time>
+                        </dd>
+                    </dl>
+                    <h4 itemprop="name">
+                        <a href="#" itemprop="url">Idén is láthattuk a medvét!</a>
+                    </h4>
+                </div>
+
+                <p>A XX. jubileumi <strong>Medve szabadtéri matematikaverseny</strong> számunkra több ok miatt is rendhagyó volt.</p>
+            </article>
+        </div>
+        <div class="col-sm-3">
+            <article class="item column-2 item-featured" itemprop="blogPost" itemscope itemtype="http://schema.org/BlogPosting">
+                <div class="entry-image intro-image" style="height: 140px; max-width: 100%; overflow: hidden;">
+                    <a href="#">
+                        <img src="/images/szepkartya.jpg" alt="" itemprop="thumbnailUrl" style="max-width: 100%; height: auto;" />
+                    </a>
+                </div>
+                <div class="entry-header">
+                    <dl class="article-info">
+                        <dt class="article-info-term"></dt>
+                        <dd class="create">
+                            <i class="fa fa-clock-o"></i>
+                            <time datetime="2019-04-14T12:30:37+02:00" itemprop="dateCreated" data-toggle="tooltip" title="Létrehozás dátuma">2019. április 14.</time>
+                        </dd>
+                    </dl>
+                    <h4 itemprop="name">
+                        <a href="#" itemprop="url">Étkezési térítési díjak</a>
+                    </h4>
+                </div>
+
+                <p>Tisztelt Szülők!&nbsp;Tájékoztatjuk Önöket, hogy lehetőség van az étkezési térítési díjak befizetésére SZÉP kártyával és bankkártyával is.</p>
+            </article>
+        </div>
+        <div class="col-sm-3">
+            <article class="item column-3 item-featured" itemprop="blogPost" itemscope itemtype="http://schema.org/BlogPosting">
+                <div class="entry-image intro-image" style="height: 140px; max-width: 100%; overflow: hidden;">
+                    <a href="#">
+                        <img src="/images/tancsicsnapok2019kep.jpg" alt="" itemprop="thumbnailUrl" style="max-width: 100%; min-height: 100%; height: auto;" />
+                    </a>
+                </div>
+                <div class="entry-header">
+                    <dl class="article-info">
+                        <dt class="article-info-term"></dt>
+                        <dd class="create">
+                            <i class="fa fa-clock-o"></i>
+                            <time datetime="2019-04-09T13:18:22+02:00" itemprop="dateCreated" data-toggle="tooltip" title="Létrehozás dátuma">2019. április 09.</time>
+                        </dd>
+                    </dl>
+                    <h4 itemprop="name">
+                        <a href="#" itemprop="url">Táncsics-napok 2019</a>
+                    </h4>
+                </div>
+
+                <p>A Táncsics-napok 2019 programjai között reméljük, ki-ki megtalálja a számára hasznos találkozást: egy új hivatással, egy színes egyéniséggel.</p>
+            </article>
+        </div>
+        <div class="col-sm-3">
+            <article class="item column-4 item-featured" itemprop="blogPost" itemscope itemtype="http://schema.org/BlogPosting">
+                <div class="entry-image intro-image" style="height: 140px; max-width: 100%; overflow: hidden;">
+                    <a href="#">
+                        <img src="/images/szemetvadaszok.jpg" alt="" itemprop="thumbnailUrl" style="max-width: 100%; min-height: 100%; height: auto;" />
+                    </a>
+                </div>
+                <div class="entry-header">
+                    <dl class="article-info">
+                        <dt class="article-info-term"></dt>
+                        <dd class="create">
+                            <i class="fa fa-clock-o"></i>
+                            <time datetime="2019-04-09T13:45:26+02:00" itemprop="dateCreated" data-toggle="tooltip" title="Létrehozás dátuma">2019. április 09.</time>
+                        </dd>
+                    </dl>
+                    <h4 itemprop="name">
+                        <a href="#" itemprop="url">Szemétvadászok</a>
+                    </h4>
+                </div>
+
+                <p>A <em>Dabasi Szemétvadászok</em> nevű civil szervezethez csatlakozva 2019. április 6-án megtisztítottuk Dabas-Gyón vasútállomását és környékét.</p>
+            </article>
+        </div>
+    </div>
+
+    <div class="items-row row-1 row clearfix">
+        <div class="col-sm-3">
+            <article class="item column-1 item-featured" itemprop="blogPost" itemscope itemtype="http://schema.org/BlogPosting">
+                <div class="entry-image intro-image" style="height: 140px; max-width: 100%; overflow: hidden;">
+                    <a href="#">
+                        <img src="/images/blogesszepalyazat.jpg" alt="" itemprop="thumbnailUrl" style="max-width: 100%; min-height: 100%; height: auto;" />
+                    </a>
+                </div>
+                <div class="entry-header">
+                    <dl class="article-info">
+                        <dt class="article-info-term"></dt>
+                        <dd class="create">
+                            <i class="fa fa-clock-o"></i>
+                            <time datetime="2019-04-09T13:37:27+02:00" itemprop="dateCreated" data-toggle="tooltip" title="Létrehozás dátuma">2019. április 09.</time>
+                        </dd>
+                    </dl>
+                    <h4 itemprop="name">
+                        <a href="#" itemprop="url">Blog író pályázat</a>
+                    </h4>
+                </div>
+
+                <p>A 2018/2019-est tanév Táncsics-napok rendezvénysorozata részeként a történelem munkaközösség blog- és esszéíró pályázatot hirdet.</p>
+            </article>
+        </div>
+        <div class="col-sm-3">
+            <article class="item column-2 item-featured" itemprop="blogPost" itemscope itemtype="http://schema.org/BlogPosting">
+                <div class="entry-image intro-image" style="height: 140px; max-width: 100%; overflow: hidden;">
+                    <a href="#">
+                        <img src="/images/curiematek.jpg" alt="" itemprop="thumbnailUrl" style="max-width: 100%; min-height: 100%; height: auto;" />
+                    </a>
+                </div>
+                <div class="entry-header">
+                    <dl class="article-info">
+                        <dt class="article-info-term"></dt>
+                        <dd class="create">
+                            <i class="fa fa-clock-o"></i>
+                            <time datetime="2019-04-08T13:19:34+02:00" itemprop="dateCreated" data-toggle="tooltip" title="Létrehozás dátuma">2019. április 08.</time>
+                        </dd>
+                    </dl>
+                    <h4 itemprop="name">
+                        <a href="#" itemprop="url">Matek sikereink</a>
+                    </h4>
+                </div>
+
+                <p>Múlt héten Szolnokon jártunk a Curie Matematika Emlékverseny döntőjén, ahol az ország különböző iskoláiból összesen 160 diák mérhette össze tudását.</p>
+            </article>
+        </div>
+        <div class="col-sm-3">
+            <article class="item column-3 item-featured" itemprop="blogPost" itemscope itemtype="http://schema.org/BlogPosting">
+                <div class="entry-image intro-image" style="height: 140px; max-width: 100%; overflow: hidden;">
+                    <a href="#">
+                        <img src="/images/torisiker.jpg" alt="" itemprop="thumbnailUrl" style="max-width: 100%; min-height: 100%; height: auto;" />
+                    </a>
+                </div>
+                <div class="entry-header">
+                    <dl class="article-info">
+                        <dt class="article-info-term"></dt>
+                        <dd class="create">
+                            <i class="fa fa-clock-o"></i>
+                            <time datetime="2019-04-03T19:51:11+02:00" itemprop="dateCreated" data-toggle="tooltip" title="Létrehozás dátuma">2019. április 03.</time>
+                        </dd>
+                    </dl>
+                    <h4 itemprop="name">
+                        <a href="#" itemprop="url">Történelemverseny</a>
+                    </h4>
+                </div>
+
+                <p>A Kosáry Domokos Történelemverseny országos fordulóján <strong>Márkus Liza</strong> a kilencedikes kategóriában 11. helyen végzett.</p>
+            </article>
+        </div>
+        <div class="col-sm-3">
+            <article class="item column-4 item-featured" itemprop="blogPost" itemscope itemtype="http://schema.org/BlogPosting">
+                <div class="entry-image intro-image" style="height: 140px; max-width: 100%; overflow: hidden;">
+                    <a href="#">
+                        <img src="/images/vadvilag.jpg" alt="" itemprop="thumbnailUrl" style="max-width: 100%; min-height: 100%; height: auto;" />
+                    </a>
+                </div>
+                <div class="entry-header">
+                    <dl class="article-info">
+                        <dt class="article-info-term"></dt>
+                        <dd class="create">
+                            <i class="fa fa-clock-o"></i>
+                            <time datetime="2019-04-03T19:43:50+02:00" itemprop="dateCreated" data-toggle="tooltip" title="Létrehozás dátuma">2019. április 03.</time>
+                        </dd>
+                    </dl>
+                    <h4 itemprop="name">
+                        <a href="#" itemprop="url">Vadvilág Fotópályázat</a>
+                    </h4>
+                </div>
+
+                <p>A Táncsics Mihály Gimnázium Öveges Laborja által a Vadvilág Világnapja alkalmából meghirdetett fotópályázatra összesen 77 kép érkezett.</p>
+            </article>
+        </div>
+    </div>
+</div>
