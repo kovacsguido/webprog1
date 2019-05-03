cfc8969773b794e76a8aa16b35c1e18c1b99adc3
Guido Kovacs <kovacs.guido@gmail.com>
2019-05-03 07:26:48 +0200

    Implemented the video page

--- templates/videos.tpl.php
+++ templates/videos.tpl.php
@@ -1,3 +1,22 @@
 <h1>Videók</h1>
 <hr class="my-4">
-<p>...</p>
+<div class="container">
+    <div class="row">
+        <div class="col-sm">
+            <h2>Beágyazott videó (Youtube)</h2>
+            <div class="d-flex justify-content-center">
+                <iframe width="500" height="315" src="https://www.youtube.com/embed/T5Aq7cRc-mU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+        </div>
+        <div class="col-sm">
+            <h2>Lokális videó</h2>
+            <div class="d-flex justify-content-center">
+                <video width="500" height="315" controls>
+                    <source src="videos/samplevideo.mp4" type="video/mp4">
+                    Sajnáljuk, a te böngésződ nem támogatja a HTML5 videót.
+                </video>
+            </div>
+        </div>
+
+    </div>
+</div>
